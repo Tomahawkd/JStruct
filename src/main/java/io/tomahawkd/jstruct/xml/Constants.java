bcf8a09def4d3949cf641dcb23a17d597b849ea5
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2021-09-29 18:27:55 +0800

    add definitions

---
+++ src/main/java/io/tomahawkd/jstruct/xml/Constants.java
@@ -0,0 +1,49 @@
+package io.tomahawkd.jstruct.xml;
+
+public class Constants {
+
+    public static final String XSD_VALIDATION_FILE = "jstruct-definitions.xsd";
+
+    // types
+    public static final String TYPE_BOOLEAN = "boolean";
+    public static final String TYPE_BYTE = "byte";
+    public static final String TYPE_INT2 = "int2";
+    public static final String TYPE_INT4 = "int4";
+    public static final String TYPE_INT8 = "int8";
+    public static final String TYPE_UINT2 = "uint2";
+    public static final String TYPE_UINT4 = "uint4";
+    public static final String TYPE_UINT8 = "uint8";
+    public static final String TYPE_BIGINT = "biginteger";
+    public static final String TYPE_BYTEARRAY = "bytearray";
+    public static final String TYPE_PADDING = "padding";
+    public static final String TYPE_LENGTH = "length";
+    public static final String TYPE_STRING = "string";
+
+    // orders
+    public static final String BYTEORDER_BIG = "big";
+    public static final String BYTEORDER_LITTLE = "little";
+    public static final String BYTEORDER_DEFAULT = "default";
+
+    // elements
+    public static final String ELEMENT = "element";
+    public static final String ELEMENT_NAME ="name";
+    public static final String ELEMENT_OFFSET = "offset";
+    public static final String ELEMENT_LENGTH = "length";
+    public static final String ELEMENT_MINLENGTH = "minLength";
+    public static final String ELEMENT_MAXLENGTH = "maxLength";
+    public static final String ELEMENT_TYPE = "type";
+    public static final String ELEMENT_CONDITION = "condition";
+
+    // block
+    public static final String BLOCK = "block";
+    public static final String BLOCK_NAME = "name";
+    public static final String BLOCK_REF = "ref";
+    public static final String BLOCK_BYTEORDER = "byteOrder";
+
+    // sequence
+    public static final String SEQUENCE = "sequence";
+    public static final String SEQUENCE_NAME = "name";
+
+    // definition
+    public static final String DEFINITION = "definition";
+}
